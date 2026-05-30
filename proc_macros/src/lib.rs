d712400e998f4c2e04c1a042c37c2c6607136795
Warhorst <46809804+Warhorst@users.noreply.github.com>
2026-05-30 10:33:29 +0200

    Added doc to the EnumValues derive macro

--- proc_macros/src/lib.rs
+++ proc_macros/src/lib.rs
@@ -2,6 +2,8 @@ use proc_macro::TokenStream;
 
 mod derive_enum_values;
 
+/// Generates an implementation of EnumValues for the item. This is only supported
+/// on enums which only consist of unit variants.
 #[proc_macro_derive(EnumValues)]
 pub fn derive_enum_values(item: TokenStream) -> TokenStream {
     derive_enum_values::create(item)
